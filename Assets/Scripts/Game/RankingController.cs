a498c87fb379a72099a52586d34203e337c64f75
林遼平 <c06955960c@gmail.com>
2016-02-15 17:35:33 +0900

    HishScoreとmobile backendを繋げるための、初期段階。

---
+++ Assets/Scripts/Game/RankingController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using NCMB;
+using System.Collections.Generic;
+
+public class RankingController : MonoBehaviour {
+
+	[SerializeField]
+	int listLimit;
+
+	/// <summary>
+	/// スコアの保存.
+	/// </summary>
+	void HighScoreSetter(string playerName, int score){
+		NCMBObject obj = new NCMBObject ("HighScore");
+		obj ["Name"] = playerName;
+		obj ["Score"] = score;
+		obj.SaveAsync ();
+	}
+
+	/// <summary>
+	/// ランキングの取得.
+	/// </summary>
+	void HighScoreGetter() {
+		NCMBQuery<NCMBObject> query = new NCMBQuery<NCMBObject> ("HighScore");
+
+		// Scoreフィールドの降順でデータを取得.
+		query.OrderByDescending("Score");
+
+		// 検索件数を設定.
+		query.Limit = listLimit;
+
+		// データストアで検索を行う.
+		query.FindAsync (( List<NCMBObject> objList, NCMBException e) => {
+			if ( e != null) {
+				// 検索失敗時の処理.
+			} else {
+				// 検索成功時の処理.
+			}
+		});
+	}
+}
---
+++ Assets/Scripts/Game/UserAuth.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using NCMB;
+using System.Collections.Generic;
+
+public class UserAuth : SingletonMonoBehaviour<UserAuth> {
+
+	private string currentPlayerName;
+
+	// mobile backendに接続してログイン.
+	public void LogIn(string id, string pw) {
+		NCMBUser.LogInAsync(id, pw, (NCMBException e) => {
+			// 接続成功したら.
+			if (e == null) {
+				currentPlayerName = id;
+			}
+		});
+	}
+
+	// mobile backendに接続して新規会員登録(メール有).
+	public void SignUp(string id, string mail, string pw) {
+		NCMBUser user = new NCMBUser ();
+		user.UserName = id;
+		user.Email = mail;
+		user.Password = pw;
+		user.SignUpAsync ((NCMBException e) => {
+			if (e == null) {
+				currentPlayerName = id;
+			}
+		});
+	}
+
+	// mobile backendに接続して新規会員登録(メール無).
+	public void SignUp(string id, string pw) {
+		NCMBUser user = new NCMBUser ();
+		user.UserName = id;
+		user.Password = pw;
+		user.SignUpAsync ((NCMBException e) => {
+			if (e == null) {
+				currentPlayerName = id;
+			}
+		});
+	}
+
+
+	// mobile backendに接続してログアウト.
+	public void LogOut() {
+		NCMBUser.LogOutAsync ((NCMBException e) => {
+			if (e == null) {
+				currentPlayerName = null;
+			}
+		});
+	}
+
+	// 現在のプレイヤー名を返す.
+	public string CurrentPlayer() {
+		return currentPlayerName;
+	}
+}
+
+/*
+ * SignUpAsyncメソッドでメールアドレスの入力は必須ではない.
+ * メールアドレスを登録するメリット.
+ * ・パスワードのリセットができる.
+ * ・登録時に本人確認のメールの送信ができる.
+ */
