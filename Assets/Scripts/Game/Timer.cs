9dc5cf394be3d739646f76b32cb0ca45060330ab
林遼平 <c06955960c@gmail.com>
2016-02-16 09:30:05 +0900

    なんか色々追加ー
    適当でごめんなさい...

---
+++ Assets/Scripts/Game/Timer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Timer.
+/// 背景とか変える人.
+/// </summary>
+
+public class Timer : MonoBehaviour {
+	static int time = 0;
+	[SerializeField]
+	int lapTime;
+	[SerializeField]
+	Animator backgroundAnimator;
+
+
+	IEnumerator Start () {
+		while (true) {
+			switch (GameManager.Instance.state) {
+			case GameManager.State.WAIT:  // 配置中.
+				// とりあえず、ソッコーでPlayへ.
+				PlayGame();
+				break;
+			case GameManager.State.PLAY: // タワーディフェンス中.
+
+				if (time >= lapTime) { // ラップタイムを超えたら.
+					time = lapTime;
+					GameManager.Instance.state = GameManager.State.RESULT;
+					backgroundAnimator.SetBool ("play", false);
+					Application.LoadLevelAdditive ("_SubResult");
+				}
+
+				// 時間を進める.
+				time++;
+				Debug.Log (time);
+
+				yield return new WaitForSeconds (1.0f);
+				break;
+			case GameManager.State.RESULT: // 小リザルト中.
+				break;
+			}
+			yield return 0;
+		}
+	}
+
+
+	public void PlayGame() {
+		backgroundAnimator.SetBool ("play", true);
+		GameManager.Instance.waveCount++;
+		GameManager.Instance.state = GameManager.State.PLAY;
+	}
+
+
+	/// <summary>
+	/// タイマーを戻す.
+	/// </summary>
+	public void TimerReset() {
+		time = 0;
+		GameManager.Instance.state = GameManager.State.WAIT;
+		Application.UnloadLevel ("_SubResult");
+	}
+}
