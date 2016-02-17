8e2ae37cdbfe62387bc9b7d5e3472161d6ddd827
YukiHayashi0208 <seiroise@gmail.com>
2016-02-17 19:29:48 +0900

    画作り

---
+++ Assets/Scripts/Game/Enemy/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 敵
+/// </summary>
+public class Enemy : MonoBehaviour {
+
+	public string targetTag = "Ruin";
+	[Range(1, 20)]
+	public int hp = 2;
+	private int nowHp;
+	[Range(0.1f, 10f)]
+	public float velocity = 1f;
+	[Header("移動する方向")]
+	public Vector3 direction;
+
+#region MonoBehaviourEvent
+	private void Update() {
+		Move();
+	}
+	private void OnCollisionEnter(Collision co) {
+		if(targetTag.Equals(targetTag)) TargetHit();
+	}
+#endregion
+#region Function
+	private void Move() {
+		transform.position += direction * velocity * Time.deltaTime;
+	}
+	private void TargetHit() {
+		Destroy(gameObject);
+	}
+#endregion
+}
---
+++ Assets/Scripts/Game/Enemy/EnemyEmitter.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 敵の生成
+/// </summary>
+public class EnemyEmitter : MonoBehaviour {
+	public GameObject[] prefabs;
+}
