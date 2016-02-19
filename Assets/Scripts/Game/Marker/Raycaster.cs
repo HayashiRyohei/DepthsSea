739639b18fbe3ba94ed23e2506807268b0b25e2f
林遼平 <c06955960c@gmail.com>
2016-02-19 11:32:23 +0900

    マーカーを追加. タグにMarkerを追加.

---
+++ Assets/Scripts/Game/Marker/Raycaster.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Rayを飛ばす.
+/// </summary>
+
+public class Raycaster : MonoBehaviour {
+	[SerializeField]
+	LayerMask layerMask;
+	[SerializeField]
+	GameObject unitInfoPanel;
+
+	void Update() {
+		// if (GameManager.Instance.state == GameManager.State.WAIT) {
+			if (Input.GetMouseButtonDown(0)) {
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit = new RaycastHit ();
+				if (Physics.Raycast(ray , out hit, layerMask)) {
+					unitInfoPanel.SetActive (true);
+				}
+			}
+		// }
+	}
+
+}
